b35e4f64a4ca67f64da6ed825272d0547780aed0
gabrielle978 <gbassiquete@outlook.com>
2026-01-06 19:16:07 -0300

    First class - Position

---
+++ src/main/java/chess/com/udemy/Main.java
@@ -0,0 +1,10 @@
+package chess.com.udemy;
+
+import chess.com.udemy.boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position pos = new Position(2, 3);
+        System.out.println(pos);
+    }
+}
