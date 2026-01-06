f2684e909c539cc96f96d040bdeb6d72376ec5a5
gabrielle978 <gbassiquete@outlook.com>
2026-01-06 19:30:51 -0300

    Starting to implement Board and Piece

--- src/main/java/chess/com/udemy/Main.java
+++ src/main/java/chess/com/udemy/Main.java
@@ -1,10 +1,10 @@
 package chess.com.udemy;
 
+import chess.com.udemy.boardgame.Board;
 import chess.com.udemy.boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position pos = new Position(2, 3);
-        System.out.println(pos);
+        Board board = new Board(8,8);
     }
 }
--- src/main/java/chess/com/udemy/boardgame/Position.java
+++ src/main/java/chess/com/udemy/boardgame/Position.java
@@ -1,7 +1,7 @@
 package chess.com.udemy.boardgame;
 
 public class Position {
-    private int row;
+    private int row; //Encapsulamento
     private int column;
 
     public Position(int row, int column) {
