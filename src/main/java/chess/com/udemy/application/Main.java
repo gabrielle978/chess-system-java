e2b7c44da68a1871410213b885ad88411959cb78
gabrielle978 <gbassiquete@outlook.com>
2026-01-20 20:04:53 -0300

    Specialmove -> Promotion

--- src/main/java/chess/com/udemy/application/Main.java
+++ src/main/java/chess/com/udemy/application/Main.java
@@ -27,14 +27,20 @@ public class Main {
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+
                 if(capturedPiece != null){
                     captured.add(capturedPiece);
+                }
+
+                if (chessMatch.getPromoted() != null){
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
 
             }
